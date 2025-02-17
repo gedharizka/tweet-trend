0f68572caeb6247a35ad3e19bcee3c8944fb3437
gedharizka <gedharizka@gmail.com>
2025-02-18 00:09:33 +0700

    Update RepositoryDetailsController.java

--- src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
+++ src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
@@ -34,7 +34,7 @@ public class RepositoryDetailsController {
 	public String getRepos() throws IOException {
 		GitHub github = new GitHubBuilder().withPassword("valaxytech@gmail.com", "XXXXXXXX").build();
 		GHRepositorySearchBuilder builder = github.searchRepositories();
-		return "Hallo From SpringBoot";
+		return "Hallo From SpringBoot - Test AKU LAKU";
 	}
 
 	@GetMapping("/trends")
